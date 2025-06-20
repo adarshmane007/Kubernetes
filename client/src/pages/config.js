545b90ddab4fd3f2710f068bf84bc99043e0a454
adarshmane007 <121188276+adarshmane007@users.noreply.github.com>
2025-06-20 19:26:46 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = "http://3.87.228.149:84";
-const API_BASE_URL = "http://app.manes.life";
+const API_BASE_URL = "http://54.172.247.21:84";
 // export default API_BASE_URL;
 // const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
 /// export default API_BASE_URL;
